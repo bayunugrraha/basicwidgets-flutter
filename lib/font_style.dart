502c23d80cb22efaabfad5aa4bc4ba21171c8e10
Bayu <bayungraha@gmail.com>
2020-11-11 20:20:16 +0700

    remove cache & custom fonts

--- lib/font_style.dart
+++ lib/font_style.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
-TextStyle mainHeader =
-    TextStyle(fontSize: 26, color: Colors.blueGrey, fontFamily: 'Poppins');
+TextStyle mainHeader = TextStyle(
+    fontSize: 26,
+    color: Colors.blueGrey,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700);
 
-TextStyle subHeader =
-    TextStyle(fontSize: 14, color: Colors.grey, fontFamily: 'Poppins');
+TextStyle subHeader = TextStyle(
+    fontSize: 14,
+    color: Colors.grey,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w300);
