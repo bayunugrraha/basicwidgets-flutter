708d290776faa19af88d9ff716bc01e92bd9aa17
Bayu <bayungraha@gmail.com>
2020-11-11 19:41:26 +0700

    margin & padding

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,9 @@ class MyApp extends StatelessWidget {
           ),
           body: SafeArea(
               child: Container(
+            color: Colors.blueAccent,
+            margin: EdgeInsets.only(left: 45.0, top: 0, right: 0, bottom: 0),
+            padding: EdgeInsets.only(left: 0, top: 50.0, right: 0, bottom: 0),
             child: Column(
               children: <Widget>[
                 Image(
