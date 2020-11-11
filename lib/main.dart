26afc91d169a8cbaa5cf9f64229694d24474fdc4
Bayu <bayungraha@gmail.com>
2020-11-11 20:14:26 +0700

    axisalignment property

--- lib/main.dart
+++ lib/main.dart
@@ -21,32 +21,38 @@ class MyApp extends StatelessWidget {
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         home: Scaffold(
-          appBar: AppBar(
-            title: Text("Cashy"),
-            backgroundColor: Colors.blueAccent,
-          ),
-          body: SafeArea(
-              child: Container(
-            margin: EdgeInsets.only(left: 23.0, top: 0, right: 23.0, bottom: 0),
-            padding: EdgeInsets.only(left: 0, top: 50.0, right: 0, bottom: 0),
-            child: Column(
-              children: <Widget>[
-                Image(
-                  image: AssetImage('assets/images/ic_payment.png'),
-                  height: 200,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 20.0, bottom: 4.0),
-                  child: Text("Rich Together", style: mainHeader),
-                ),
-                Text(
-                  "Save your money little bit and we \nwill help to be rich.",
-                  style: subHeader,
-                  textAlign: TextAlign.center,
-                )
-              ],
+            appBar: AppBar(
+              title: Text("Cashy"),
+              backgroundColor: Colors.blueAccent,
             ),
-          )),
-        ));
+            body: SafeArea(
+                child: Container(
+              margin:
+                  EdgeInsets.only(left: 23.0, top: 0, right: 23.0, bottom: 0),
+              padding: EdgeInsets.only(left: 0, top: 0, right: 0, bottom: 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image(
+                        image: AssetImage('assets/images/ic_payment.png'),
+                        height: 200,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0, bottom: 4.0),
+                        child: Text("Rich Together", style: mainHeader),
+                      ),
+                      Text(
+                        "Save your money little bit and we \nwill help to be rich.",
+                        style: subHeader,
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ))));
   }
 }
